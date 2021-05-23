c7023a8cbf5f62edea40793caab3bef84eef0b12
MunirVinicius <munir.vinicius11@gmail.com>
2021-05-22 21:39:40 -0300

    user create

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const app = express()
 const public = path.join (__dirname, '../public')
 const db = require('./db')
+const bcrypt = require('bcryptjs')
+const User = require('../model/user')
 
 app.set('views', path.join (__dirname, '../views'));
 app.set('view engine', 'pug')
@@ -15,7 +17,40 @@ app.get('/',(request,response)=>{
 })
 
 app.post('/register', async (request,response)=>{
-    console.log(request.body)
+    const {username,email, password: plainTextPassword} = request.body;
+
+    if(!email || typeof email !== 'string'){
+        return response.json({status:'error', error: 'Invalid email'})
+    }
+
+    if(plainTextPassword.length <= 5){
+        return response.json({status:'error', error: 'Your password should have atleast 6 characters'})
+    }
+
+    if(!plainTextPassword || typeof plainTextPassword !== 'string'){
+        return response.json({status:'error', error: 'Invalid password'})
+    }
+
+    if(!username || typeof username !== 'string'){
+        return response.json({status:'error', error: 'Invalid username'})
+    }
+
+    const password = await bcrypt.hash(plainTextPassword,10)
+
+    try {
+        const response = await User.create({
+            username,
+            email,
+            password
+        })
+        console.log('User created', response)
+    } catch (error) {
+        if(error.code === 11000){
+            return response.json({status: 'error', error:'Email already used ...'})
+        }
+        throw error
+    }
+    return response.json({status: 'ok'})
 })
 
 app.listen(3000,()=>{
--- backend/src/db.js
+++ backend/src/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const User = require('../model/user')
 mongoose.connect('mongodb://localhost:27017/topdesign',{
     useNewUrlParser:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+    useCreateIndex:true
 })
 
 
